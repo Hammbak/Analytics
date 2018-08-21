47feab3df4bac46a97ac54794b73fd1ff08bbad9
Hammbak <hammbakie@gmail.com>
2018-08-21 09:00:27 +0900

    .

---
+++ Analytics/Models/homona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Analytics.Models
+{
+    public class homona
+    {
+        public string Gurum { get; set; } = string.Empty;
+        public string lowGurum { get; set; }
+
+        public void Run(int k)
+        {
+            int i = 0;
+            int c = 0;
+            for (; i < 10; i++)
+            {
+                c++;
+            }
+        }
+    }
+}
